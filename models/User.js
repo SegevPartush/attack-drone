e1b587b6f85acfedc0bcd63a8c5f1648313bf399
BarNoy123 <129206470+BarNoy123@users.noreply.github.com>
2024-11-03 21:28:23 +0200

    added models

---
+++ models/User.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema(
+    {
+        firstName: { type: String, required: true },
+        lastName: { type: String, required: true },
+        email: { type: String, required: true, min: 6, max: 255},
+        password: { type: String, required: true, max: 1024, min: 6, },
+        role: { type: String, enum: ['admin', 'user'], required: true },
+        lastLogin: {
+            type: Date,
+        },
+    },
+    {
+        timestamps: {
+            createdAt: 'createdAt',
+            updatedAt: 'updatedAt',
+        },
+    }
+);
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = { User } 
