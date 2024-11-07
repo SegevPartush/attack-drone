9433ee4669520b1828d7ed017b1f7b27a28ac0e9
Ofirrazz <ofirraz1998@gmail.com>
2024-11-07 20:22:31 +0200

    add css explore and homeheader

--- utils/upload.js
+++ utils/upload.js
@@ -17,6 +17,7 @@ const storage = new GridFsStorage({
             filename: `${Date.now()}-product-${file.originalname}`,
         };
     },
+    //sdfsdf
 });
 
 module.exports = multer({ storage });
